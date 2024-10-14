5c2792ef4bce7d3fdb74a94d635faaf3ec8c4efc
Saurav <saurav.jaswl@gmail.com>
2024-10-14 21:09:36 +0545

    setting up unsplash API url and usequery setup

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=VWx9kb494dzJwtjMI5fuailyK028Zzbd_CC-AFI3E4k&query=office";
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
   return <h2>Gallery</h2>;
 };
 export default Gallery;
